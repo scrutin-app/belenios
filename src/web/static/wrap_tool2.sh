4b13551d5b594351d1a4384cb1e83543ef84b8b5
Maxime Lalisse <maxime.lalisse@gmail.com>
2023-01-28 18:29:01 +0545

    feat: patch js_of_ocaml output for mobile

--- src/web/static/wrap_tool2.sh
+++ src/web/static/wrap_tool2.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "\"use strict\";(function(g){var belenios={};"
-sed "s/(function(){return this}())/(g)/g" "$1"
+#sed "s/(function(){return this}())/(g)/g" "$1"
+cat $1 | sed "s/(function(){return this}())/(g)/g" | sed 's/require("fs")/{}/g' | sed 's/require("constants")/{}/g'
 echo
 echo "}(this));"
